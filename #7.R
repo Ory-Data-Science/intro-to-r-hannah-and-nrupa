2bc3a75b418b0462169db6a4796452c090aea2aa
Kren <Hannah.Kren@stlcop.local>
2017-10-15 17:41:23 -0500

    #7

---
+++ #7.R
@@ -0,0 +1,16 @@
+#7
+> number_of_birds <- c(28, 32, 1, 0, 10, 22, 30, 19, 145, 27, 36, 25, 9, 38, 21, 12, 122, 87, 36, 3, 0, 5, 55, 62, 98, 32, 900, 33, 14, 39, 56, 81, 29, 38, 1, 0, 143, 37, 98, 77, 92, 83, 34, 98, 40, 45, 51, 17, 22, 37, 48, 38, 91, 73, 54, 46, 102, 273, 600, 10, 11)
+> print(length(number_of_birds))
+[1] 61
+> print(number_of_birds[42])
+[1] 83
+> print(last(number_of_birds))
+[1] 11
+> print(sum(number_of_birds))
+[1] 4366
+> print(min(number_of_birds))
+[1] 0
+> print(max(number_of_birds))
+[1] 900
+> print(mean(number_of_birds))
+[1] 71.57377
